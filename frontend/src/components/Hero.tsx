0ead1630d26197d72ed0cbcc2ba26c794f646e4e
Sanchit Singhal <sanchit.singhal2811@gmail.com>
2024-02-10 16:00:20 +0530

    Adding login-register functionality

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-white text-4xl font-bold">Where Every Stay Counts</h1>
+        <p className="text-white text-2xl">Your Reliable Gateway to Effortless Hotel Bookings.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
